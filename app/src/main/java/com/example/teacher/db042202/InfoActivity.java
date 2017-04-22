8aaf8267ab9169773a6d06e2b915e7c67e76dcb9
Shaojung <shaojung@gmail.com>
2017-04-22 16:07:05 +0800

    reg ok

--- app/src/main/java/com/example/teacher/db042202/InfoActivity.java
+++ app/src/main/java/com/example/teacher/db042202/InfoActivity.java
@@ -3,14 +3,38 @@ package com.example.teacher.db042202;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class InfoActivity extends AppCompatActivity {
     String uid;
+    TextView tvNickname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         uid = getIntent().getStringExtra("uid");
+        tvNickname = (TextView) findViewById(R.id.tvNickname);
         Log.d("FCM", "uid:" + uid);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(uid);
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tvNickname.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
