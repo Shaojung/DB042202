86245c19d3ffaa170928d1c8fa0e303f8b29b694
Shaojung <shaojung@gmail.com>
2017-04-22 15:33:35 +0800

    reg ok

--- app/src/main/java/com/example/teacher/db042202/InfoActivity.java
+++ app/src/main/java/com/example/teacher/db042202/InfoActivity.java
@@ -2,6 +2,7 @@ package com.example.teacher.db042202;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class InfoActivity extends AppCompatActivity {
     String uid;
@@ -10,6 +11,6 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         uid = getIntent().getStringExtra("uid");
-
+        Log.d("FCM", "uid:" + uid);
     }
 }
